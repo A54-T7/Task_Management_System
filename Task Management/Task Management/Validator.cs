c96fec62ed90373fc4c793a0120f7c83dd9cf92d
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2023-10-11 12:33:29 +0300

    Add StringRange Validator + InvalidUserInputException

--- Task Management/Task Management/Validator.cs
+++ Task Management/Task Management/Validator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task_Management.Exceptions;
 
 namespace Task_Management
 {
-    internal class Validator
+    public static class Validator
     {
+        public static void ValidateStringRange(string value, int min, int max, string message)
+        {
+            if (value.Length < min || value.Length > max)
+            {
+                throw new InvalidUserInputException(message);
+            }
+        }
     }
 }

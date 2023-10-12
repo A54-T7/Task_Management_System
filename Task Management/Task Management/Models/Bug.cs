5fe88bb0c96ba75c0514f97d1e35d92e798ad011
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2023-10-12 22:20:26 +0300

    Add Bug + IBug

--- Task Management/Task Management/Models/Bug.cs
+++ Task Management/Task Management/Models/Bug.cs
@@ -3,24 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task_Management.Models.Contracts;
+using Task_Management.Models.Enums;
 
 namespace Task_Management.Models
 {
-    internal class Bug
+    public class Bug : Task, IBug
     {
-        
-/* Bugs must have an ID, a title, a description, a list of steps to reproduce it, a priority, a
-severity, a status, an assignee, a list of comments and a list of changes history.
-    • Title is a string between 10 and 50 symbols.
-    • Description is a string between 10 and 500 symbols.
-    • Steps to reproduce is a list of strings.
-    • Priority is one of the following: High, Medium, or Low.
-    • Severity is one of the following: Critical, Major, or Minor.
-    • Status is one of the following: Active or Fixed.
-    • Assignee is a member from the team.
-    • Comments is a list of comments (string messages with an author).
-    • History is a list of all changes (string messages) that were done to the bug. */
-        
 
+        private List<string> steps = new List<string>();
+        private PriorityType priority;
+        private SeverityType severity;
+        private BugStatusType status;
+
+
+
+        public Bug(string title, string description, PriorityType priority, SeverityType severity, BugStatusType status) : base(title, description)
+        {
+
+            Priority = priority;
+            Severity = severity;
+            BugStatus = status;
+
+        }
+
+        public IList<string> Steps
+        {
+            get
+            {
+                return new List<string>(steps);
+            }
+        }
+
+        public PriorityType Priority
+        {
+            get
+            {
+                return priority;
+            }
+            private set
+            {
+                priority = value;
+            }
+        }
+
+        public SeverityType Severity
+        {
+            get
+            {
+                return severity;
+            }
+            private set
+            {
+                severity = value;
+            }
+        }
+
+        public BugStatusType BugStatus
+        {
+            get
+            {
+                return status;
+            }
+            private set
+            {
+                status = value;
+            }
+        }
+
+
+
+        //    * Bugs must have an ID, a title, a description, a list of steps to reproduce it, a priority, a
+        //            severity, a status, an assignee, a list of comments and a list of changes history.
+        //    • Assignee is a member from the team.
     }
 }
--- Task Management/Task Management/Models/Contracts/IBug.cs
+++ Task Management/Task Management/Models/Contracts/IBug.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task_Management.Models.Enums;
 
 namespace Task_Management.Models.Contracts
 {
-    public interface IBug
+    public interface IBug : ITask
     {
+        IList<string> Steps { get; }
+
+        PriorityType Priority { get; }
+
+        SeverityType Severity { get; }
+
+        BugStatusType Status { get; }
+
+        //ToDo
+        //Assignee
     }
 }

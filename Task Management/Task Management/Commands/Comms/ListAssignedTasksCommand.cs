f240473c55c97f881496601a2c2e2e707f90212d
Stanimir <staanimir@gmail.com>
2023-10-22 10:59:15 +0300

    Implement ListAssignedTasks command.

--- Task Management/Task Management/Commands/Comms/ListAssignedTasksCommand.cs
+++ Task Management/Task Management/Commands/Comms/ListAssignedTasksCommand.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Task_Management.Core.Contracts;
 using Task_Management.Exceptions;
+using Task_Management.Models.Contracts;
 
 namespace Task_Management.Commands.Comms
 {
@@ -30,11 +31,87 @@ namespace Task_Management.Commands.Comms
         public string ListTasks(string keyword)
         {
             StringBuilder sb = new StringBuilder();
-            var taskList = Repository.Tasks;
 
-            throw new NotImplementedException();
+            List<IBug> bugList = new List<IBug>();
+            List<IStory> storyList = new List<IStory>();
+
+            switch (keyword)
+            {
+                case "SortByTitle":
+                    bugList = Repository.Bugs.Where(bug => bug.Assignee != null)
+                        .OrderBy(bug => bug.Assignee.Name).ToList();
+                    storyList = Repository.Stories.Where(story => story.Assignee != null)
+                        .OrderBy(story => story.Assignee.Name).ToList();                 
+
+                    break;
+                case "FilterByAssignee":
+                    string memberName = InputMember().Name;
+
+                    bugList = Repository.Bugs.Where(bug => bug.Assignee.Name == memberName).ToList();
+                    storyList = Repository.Stories.Where(story => story.Assignee.Name == memberName).ToList();
+
+                    break;
+                case "FilterByStatus":
+                    string status = InputStatus();
+
+                    bugList = Repository.Bugs.Where(bug => bug.Status.ToString() == status).ToList();
+                    storyList = Repository.Stories.Where(story => story.Status.ToString() == status).ToList();
+
+                    break;
+
+                case "FilterByStatusAndAssignee":
+                    memberName = InputMember().Name;
+                    status = InputStatus();
+
+                    bugList = Repository.Bugs.Where(bug => bug.Assignee.Name == memberName)
+                        .Where(bug => bug.Status.ToString() == status).ToList();
+                    storyList = Repository.Stories.Where(story => story.Assignee.Name == memberName)
+                        .Where(story => story.Status.ToString() == status).ToList();
+
+                    break;
+
+
+                default:
+                    throw new InvalidUserInputException("No such command!");
+            }
+
+            if (bugList != null)
+            {
+                foreach (var bug in bugList)
+                {
+                    sb.AppendLine(bug.ToString());
+                }
+            }
+            if (storyList != null)
+            {
+                foreach (var story in storyList)
+                {
+                    sb.AppendLine(story.ToString());
+                }
+            }
+            
+            if (sb.Length == 0)
+            {
+                Console.WriteLine("There are no tasks that meet the conditions!");
+            }
 
             return sb.ToString().Trim();
+        }
+
+        public IMember InputMember()
+        {
+            Console.Write(" Member to filter by - ");
+            string memberName = Console.ReadLine();
+
+            return Repository.GetMember(memberName);
+        }
+
+        public string InputStatus()
+        {
+            Console.Write(" Status to filter by - ");
+            string statusAsString = Console.ReadLine();
+
+            return statusAsString;
         }
     }
 }
